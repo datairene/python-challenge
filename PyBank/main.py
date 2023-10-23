007c089d6157cd57cb3e7af736e1be203ea27b67
datairene <ireneostephenson@gmail.com>
2023-10-22 20:58:18 -0500

    added finished work

---
+++ PyBank/main.py
@@ -0,0 +1,10 @@
+import csv
+total_months = 0
+net_total = 0
+previous_profit = None
+profit_change = 0
+greatest_increase = 0
+greatest_decrease = 0
+greatest_increase_date = ""
+greatest_decrease_date = ""
+profit_changes = []
